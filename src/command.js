e7719e21905a6874536c643448bad4bde450bf96
Cai Zhen <zhenchai0000@gmail.com>
2023-01-18 18:10:11 +0800

    update: command name

--- src/command.js
+++ src/command.js
@@ -1,10 +1,27 @@
 exports.init = (Bot, log) => {
-    Bot.onText(/\/start/,(msg) => {
+    Bot.onText(/\/help/, (msg) => {
+        log.notice('[help]', 'Executed - ' + msg.from.username);
+        return Bot.sendMessage(msg.chat.id, "Hi, @"+msg.from.username+" you can type /help");
+    });
+
+    Bot.onText(/\/start/, (msg) => {
         log.notice('[start]', 'Executed - ' + msg.from.username);
         return Bot.sendMessage(msg.chat.id, "Subscribe my OnlyFans!!! Thanks you!!! 😘");
     });
-    Bot.onText(/\/ee/,(msg) => {
-        log.notice('[start]', 'Executed - ' + msg.from.username);
+
+    Bot.onText(/\/ee/, (msg) => {
+        log.notice('[random]', 'Executed - ' + msg.from.username);
         return Bot.sendMessage(msg.chat.id, "@"+msg.from.username+" Fuck you 🖕");
+    });
+
+    Bot.on('text', (msg) => {
+        console.log(msg.text);
+        if (msg.text == 'fuck' || msg.text == 'diu') {
+            log.notice('[random]', 'Executed - ' + msg.from.username);
+            return Bot.sendMessage(msg.chat.id, "@"+msg.from.username+" Fuck you 🖕");
+        } else {
+            log.notice('[random]', 'Executed - ' + msg.from.username);
+            return Bot.sendMessage(msg.chat.id, "@"+msg.from.username+" Hi, you can type /help 🖕");
+        }
     });
 }
