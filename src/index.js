652db9f397c76fec00f4964fbcef4651311c958d
Cai Zhen <zhenchai0000@gmail.com>
2023-01-23 18:00:07 +0800

    update: error handling

--- src/index.js
+++ src/index.js
@@ -6,9 +6,13 @@ Object.defineProperty(log, 'heading', { get: () => { return new Date().toUTCStri
 
 // enable telegram bot instant
 const TelegramBot = require('node-telegram-bot-api');
-const Token = process.env.BOT_TOKEN;
+const Token = process.env.TELEGRAM_BOT_TOKEN;
 const Bot = new TelegramBot(Token, {polling: true});
 
 const command = require('./command.js');
 
 command.init(Bot, log);
+
+Bot.on('polling_error', (msg) => {
+    log.error('[Polling Error]', msg);
+});
