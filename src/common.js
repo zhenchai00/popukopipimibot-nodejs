bdfead2d61bbf45f6666490c3d5d16aa5d60f07f
caizhen <zhenchai0000@gmail.com>
2023-01-27 00:01:17 +0800

    add: common method
    - Add common method for capitalize first letter of string

--- src/common.js
+++ src/common.js
@@ -9,3 +9,11 @@ exports.getDate = () => {
     let dateObj = new Date;
     return dateObj.toISOString().split('T')[0];
 }
+
+exports.getCapitalizeFirstLetter = (string) => {
+    let arr = string.split(' ');
+    for (var i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    }
+    return arr.join(' ');
+}
